5e56bd56c5989518702554b068afa7d6a0434d1b
Felipe Escoto <felescoto95@hotmail.com>
2017-11-19 20:36:12 -0600

    Working by magic

--- assets/js/libphilip.js
+++ assets/js/libphilip.js
@@ -32,3 +32,29 @@ function downloadString (element, fileName, mime) {
   dlAnchorElem.setAttribute ("download", fileName);
   dlAnchorElem.click ();
 }
+
+function parse_query_string(query) {
+  var vars = query.split("&");
+  var query_string = {};
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+      // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+      query_string[pair[0]] = arr;
+      // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}
+
+function addParameterToURL(param){
+  _url = location.href;
+  _url += _url.split('?')[0];
+  return _url;
+}
